3986bf819c155bc668e32b424559c830599799a0
MathiasFaber <makr20ah@student.cbs.dk>
2024-02-08 14:13:16 +0100

    Finish assigment one

--- assignments/FP-01.fs
+++ assignments/FP-01.fs
@@ -7,23 +7,20 @@ let sqr x =
 // Exercise 1.2 Write a function pow : float -> float -> float so that pow x n returns xn.
 let pow (x:float) (n:float) =
     x ** n
-    
-// 1.1 Declare a function g: int -> int, where g(n) = n + 4.
-let g n =
-    n + 4
 
-// 1.2 Declare a function h: float * float -> float
+
+// 1.3 (1.2) Declare a function h: float * float -> float
 
 let h (x:float, y:float) =
     System.Math.Sqrt (x**2 + y**2)
     
-//1.3 Write function expressions corresponding to the functions g and h in the exercises 1.1 and 1.2.
+//1.4 (1.3) Write function expressions corresponding to the functions g and h in the exercises 1.1 and 1.2.
 // g
 fun n -> n +4
 // h
 fun (x:float,y:float) -> System.Math.Sqrt (x**2 + y**2)
 
-(*1.4 Declare a recursive function f: int -> int, where
+(*1.5 (1.4) Declare a recursive function f: int -> int, where
 f(n) = 1+2+···+(n−1)+n
 for n ≥ 0. (Hint: use two clauses with 0 and n as patterns.) State the recursion formula corresponding to the declaration. Give an evaluation for f(4).*)
 let rec f n =
@@ -31,7 +28,7 @@ let rec f n =
     | 0 -> n
     | _ -> + n + f (n - 1)
 
-(*1.5 The sequence F0 , F1 , F2 , . . . of Fibonacci numbers is defined by:
+(*1.6 (1.5) The sequence F0 , F1 , F2 , . . . of Fibonacci numbers is defined by:
 F0 = 0 F1 = 1
 Fn = Fn−1 + Fn−2
 Thus, the first members of the sequence are 0, 1, 1, 2, 3, 5, 8, 13, . . ..
@@ -46,7 +43,7 @@ let rec fibonacci n =
 let fResult = f 4
 let fibonacciResult = f 0
 
-(*1.6 Declare a recursive function sum: int * int -> int, where
+(* 1.7 (1.6) Declare a recursive function sum: int * int -> int, where
 sum(m, n) = m + (m + 1) + (m + 2) + · · · + (m + (n − 1)) + (m + n)
 for m ≥ 0 and n ≥ 0. (Hint: use two clauses with (m,0) and (m,n) as patterns.)
 Give the recursion formula corresponding to the declaration.*)
@@ -55,26 +52,33 @@ let rec sum (m:int,n:int) =
     | m,0 -> m
     | m,n -> m + n + sum (m,n-1)
     
-(*1.7 Determine a type for each of the expressions:
+(*1.8 (1.7) Determine a type for each of the expressions:
       (System.Math.PI, fact -1)
       fact(fact 4)
       power(System.Math.PI, fact 2)
-      (power, fact)*)
+      (power, fact)
+      *)
 
 // (System.Math.PI, fact -1) is a tuple of (float * int)
 // fact(fact 4) is an int
 // power(System.Math.PI, fact 2) is a float
 // (power, fact) is a (float * int -> float) * (int -> int):
-// (float * int -> float) is a function taking a tuple of float and int as input and returns float
+// (float * float -> float) is a function taking a tuple of float and float as input and returns float
 // (int -> int) is a function that takes an int and outputs an int
 
 (*
-1.8 Consider the declarations:
+1.9 (1.8) Consider the declarations:
       let a = 5;;
       let f a = a + 1;;
       let g b = (f b) + a;;
 Find the environment obtained from these declarations and write the evaluations of the expres- sions f 3 and g 3.
-*) // todo
+*) 
+
+// Environment obtained: 
+// The environment consists of the binding a = 5
+// Evaluations: 
+// f 3 = 4
+// g 3 = 9
 
 (*Exercise 1.10 Write a function dup:string->string that concatenates a string with itself.
 You can either use + or ˆ. For example:
