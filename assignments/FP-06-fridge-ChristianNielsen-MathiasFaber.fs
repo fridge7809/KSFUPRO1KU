db540b77a911e52201c52f90885c8b4a1910a3ba
cemn <127931228+fridge7809@users.noreply.github.com>
2024-03-21 10:48:43 +0100

    Solve rest of fp06

--- assignments/FP-06-fridge-ChristianNielsen-MathiasFaber.fs
+++ assignments/FP-06-fridge-ChristianNielsen-MathiasFaber.fs
@@ -1,11 +1,12 @@
 module assignments.FP_06
 
 (*
-    6.1 (6.2)
+    Exercise 6.1 HR exercise 6.2
     Postfix form is a particular representation of arithmetic expressions where each operator is preceded by its operand(s), for example:
     (x + 7.0) has postfix form x 7.0 +
     (x+7.0)∗(x−5.0) has postfix form x7.0 + x5.0 − ∗
     Declare an F# function with type Fexpr -> string computing the textual, postfix form of expression trees from Section 6.2.
+    
 *)
 
 type Fexpr = 
@@ -31,12 +32,12 @@ let rec toString = function
     | Log fe -> "(" + (toString fe) + ") log"
     | Exp fe -> "(" + (toString fe) + ") exp"
 
-// test strings:
+// Test strings:
 toString(Mul(Cos(Mul(X, X)), Add(Mul(Const 1.0, X), Mul(X, Const 1.0))));;
 toString (Sin(Mul(X, Const 7.0)))
 
 
-(*6.2 (6.8)*)
+// Exercise 6.2 HR exercise 6.8
 
 // 1
 type Instruction = | ADD | SUB | MULT | DIV | SIN | COS | LOG | EXP | PUSH of float
@@ -79,33 +80,81 @@ let intpInstr (stack: Stack) (instr: Instruction) : Stack =
         | _ -> failwith "EXP intruction failed"
     | PUSH x -> x::stack
 
-// test:
-let myStack: float list = [3.0 .. 7.0]
+// Test:
+let myStack : float list = [3.0 .. 7.0]
 intpInstr myStack ADD
 intpInstr myStack MULT
 
 
 // 2
-let intpProg (instructions: Instruction list): float =  
-    let rec helper (stack: Stack) (instructions: Instruction list): Stack = 
+let intpProg (instructions: Instruction list) : float =  
+    let rec helper (stack: Stack) (instructions: Instruction list) : Stack = 
         match instructions with
         | [] -> stack
-        | a::rest -> 
+        | a :: rest -> 
             let updatedStack = intpInstr stack a
             helper updatedStack rest
     match helper [] instructions with 
-    | [] -> 0.0 
-    | result::_ -> result
+    | [] -> failwith "Empty instruction list"
+    | result :: _ -> result
 
-// test:
+// Test:
 intpProg [PUSH 1.0; PUSH 2.0; PUSH 5.0; PUSH 6.0; ADD; MULT]
 
 
-// 3
-let trans (fe: Fexpr, x: float) (*: Instruction list*) =
-    toString fe
-    // not done and not sure how to :((((
+// 3 - traverse expresssion tree in post order.
+//   for each node, generate the corresponding instruction and append it to the list.
+let trans (fe: Fexpr, x: float) : Instruction list =
+    let rec helper (fe: Fexpr) : Instruction list = 
+        match fe with
+        | Const x -> [PUSH x]
+        | X -> [PUSH x]
+        | Add(fe1,fe2) -> helper fe1 @ helper fe2 @ [ADD]
+        | Sub(fe1,fe2) -> helper fe1 @ helper fe2 @ [SUB]
+        | Mul(fe1,fe2) -> helper fe1 @ helper fe2 @ [MULT]
+        | Div(fe1,fe2) -> helper fe1 @ helper fe2 @ [DIV]
+        | Sin fe -> helper fe @ [SIN]
+        | Cos fe -> helper fe @ [COS]
+        | Log fe -> helper fe @ [LOG]
+        | Exp fe -> helper fe @ [EXP]
+    helper fe
 
+// Exercise 6.3 HR exercise 7.2
+// Signature and implementation is combined in the same file given the constraints of the exercise (only able to upload one file)
 
+// Signature to be put in an .fsi
 
-(*6.3 (7.2)*)
+(*
+module Complex
+type RE = float
+type IM = float
+[<Sealed>]
+type Complex =
+    | Complex of RE * IM
+    static member ( + )  : Complex * Complex -> Complex
+    static member ( - )  : Complex * Complex -> Complex
+    static member ( * )  : Complex  * Complex -> Complex
+    static member ( * )  : Complex * Complex -> Complex
+*)
+
+// Implementation to be put in an .fs file
+module Complex =
+
+    type RE = float
+    type IM = float
+    type Complex =
+        | Complex of RE * IM
+        static member  (+) (c1: Complex, c2: Complex) : Complex =
+            match c1, c2 with
+            | Complex (re1, im1), Complex (re2, im2) -> Complex (re1 + re2, im1 + im2)
+        static member  (-) (c1: Complex, c2: Complex) : Complex =
+            match c1, c2 with
+            | Complex (re1, im1), Complex (re2, im2) -> Complex (re1 - re2, im1 - im2)
+        static member  (*) (c1: Complex, c2: Complex) : Complex =
+            match c1, c2 with
+            | Complex (re1, im1), Complex (re2, im2) -> Complex (re1 * re2 - im1 * im2, re1 * im2 + re2 * im1)
+        static member (/) (c1: Complex, c2: Complex) : Complex =
+            match c1, c2 with
+            | Complex (re1, im1), Complex (re2, im2) -> 
+                let denom = re2 * re2 + im2 * im2
+                Complex ((re1 * re2 + im1 * im2) / denom, (re2 * im1 - re1 * im2) / denom)
